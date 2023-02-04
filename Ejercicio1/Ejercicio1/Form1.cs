f81fde80cf941929b6b4b4514d2ea968b11f7502
Erick02x <124105953+Erick02x@users.noreply.github.com>
2023-02-03 18:58:27 -0600

    Terminado
    Ejecución separada

---
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        decimal Resultado =  0;
+       
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+            decimal numero = Convert.ToDecimal(NumeroTextBox.Text);
+            Resultado = TipoNum(numero);
+            if (Resultado % 2 == 0)
+            {
+                MessageBox.Show("El numero: " + Resultado + " Es Par", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else 
+            {
+                MessageBox.Show("El numero: " + Resultado + " Es Impar ", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            Resultado = Signo(numero);
+            if (Resultado >= 0)
+            {
+                MessageBox.Show("El numero: " + Resultado + " Positivo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El numero: " + Resultado + " Negativo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }            
+        
+        private decimal TipoNum(decimal num)
+        {
+            return num;
+        }
+
+        private decimal Signo(decimal num)
+        {
+            return num;
+        }
+    }
+}
